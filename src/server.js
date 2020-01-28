988137b4dfb5f065ca048d239a2fcf196d2fde97
Garroti <gabryelmacedo0@gmail.com>
2020-01-28 00:17:58 -0300

    variables

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
+const variables = require('./config/variables');
+
 const app = require('./app')
 
-app.listen(3001, () => {
+app.listen(variables.Api.port, () => {
     console.info(`Servidor rodando na porta 3001`);
 })
